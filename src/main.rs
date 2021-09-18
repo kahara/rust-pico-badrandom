630cc04af390963460c02be7f9d1587d02d07c94
Joni Kähärä <joni.kahara@async.fi>
2021-09-18 17:51:39 +0300

    change repo name

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::io::{self, Write};
 
 #[derive(Debug, Copy, Clone)]
 struct Lfsr {
-    start: u32,
+    start: u16,
 }
 
 impl Lfsr {
     pub fn new() -> Self {
-        Lfsr { start: 0xa5a5a5a5 }
+        Lfsr { start: 0xffff }
     }
 }
 
@@ -15,9 +15,10 @@ impl Iterator for Lfsr {
     type Item = bool;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let bit = ((self.start >> 1) ^ (self.start >> 4)) & 1;
+        let bit =
+            ((self.start >> 0) ^ (self.start >> 2) ^ (self.start >> 3) ^ (self.start >> 5)) & 1;
 
-        self.start = (self.start >> 1) | (bit << 31);
+        self.start = (self.start >> 1) | (bit << 15);
         Some((bit & 0x1) != 0)
     }
 }
@@ -28,13 +29,13 @@ fn main() -> io::Result<()> {
     let mut bit: u8 = 0x0;
     let mut buffer: [u8; 1] = [0];
     let stdout = io::stdout();
-    let mut handle = stdout.lock();
+    let mut out = stdout.lock();
 
     for x in lfsr {
         byte = byte | ((x as u8) << bit);
         if bit == 7 {
             buffer[0] = byte; // FIXME: do without?
-            handle.write(&buffer)?;
+            out.write(&buffer)?;
             byte = 0;
             bit = 0;
         } else {
